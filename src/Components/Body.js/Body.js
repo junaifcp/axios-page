88632b01263e4d45904e5bc4df8c0e81bf27b1ff
junaif <cpjunaif98@gmail.com>
2021-10-26 17:34:37 +0530

    commit done

--- src/Components/Body.js/Body.js
+++ src/Components/Body.js/Body.js
@@ -1,38 +1,39 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './Body.css'
 import axios from 'axios'
 import { useState } from 'react'
-//import Select from 'react-select'
+
 
 function Body() {
+    const [state,setState]=useState([])
     const [form1,setForm1]=useState('')
-    //const [form2,setForm2]=useState('')
-   // const [quali,setQuali]=useState('')
-   // const [course,setCourse]=useState('')
+
    const [object,setObject]=useState([])
     const submitChange=(e)=>{
         e.preventDefault()
         setObject([...object,form1])
-        axios.get('https://jsonplaceholder.typicode.com/users').then((response)=>{
-            axios.post('https://jsonplaceholder.typicode.com/users',object).then(response=>{
-                console.log(response)
-            }).catch(error=>{
-                console.log(error)
-            })
-
-        })
     }
+    useEffect(() => {
+       axios.get('https://jsonplaceholder.typicode.com/users').then((response)=>{
+           console.log(response.data)
+           setState(response.data.name)
+       }).catch(error=>{
+           console.log(error)
+       })
+    }, [])
     
     return ( 
         <div className="main-body">
             <div className='body'><br /><br />
+            <form action="post" >
                 <b>Name:</b>&nbsp;<input value={form1} onChange={(e)=>setForm1(e.target.value)} type="text" id='name' className='sdname' />&nbsp;&nbsp;&nbsp;&nbsp;<br/><br />
                 {/*<b>Qualification:</b><input value={form2} onChange={(e)=>setForm2(e.target.value)} type="text" id='s-age' className='age' /><br /><br />
                 <b>Interested Course&nbsp;:&nbsp;</b>*/ } 
                 <div className="btn-div">
                 <button className='clear'>Clear</button>
-                <button className='submit' onClick={submitChange}>Submit</button>
+                <button className='submit' onClick={submitChange}>Submit</button> 
                 </div>
+            </form>  
 
             </div>
             <div className="table">
@@ -46,7 +47,7 @@ function Body() {
 
                     return(
                             <tr>
-                                        <td>{value}</td>
+                                <td>{value}</td>
                             </tr>       
                             
                         
